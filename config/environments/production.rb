0844d71839a9de872376bb5d287a40e044ccfcaf
Benedikt Richter <richter-benedikt@t-online.de>
2020-05-10 17:40:43 +0200

    bug fixes cloudinary

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,9 +35,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :cloudinary
-
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
@@ -58,7 +55,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rails_mister_cocktail_production"
+  # config.active_job.queue_name_prefix = "airtunes_389_production"
 
   config.action_mailer.perform_caching = false
 
@@ -109,4 +106,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.active_storage.service = :cloudinary
 end
